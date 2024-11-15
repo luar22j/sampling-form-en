38cf0e199dd736481c866e58caa7ce7905f7c1e5
Luar <luarjs@gmail.com>
2024-11-15 10:39:33 +0100

    feat: add new en page

--- src/components/home/Section.tsx
+++ src/components/home/Section.tsx
@@ -1,35 +1,45 @@
 import { Link } from "react-router-dom";
-import { useTranslation } from "react-i18next";
 
 const Section = () => {
-  const { t, i18n } = useTranslation();
-  const currentLang = i18n.language;
-
   return (
     <section className="flex flex-col gap-5 my-5 lg:mt-[100px]">
       <article className="flex flex-col gap-5 mx-5 justify-center lg:items-center sm:mx-[100px] lg:mx-[150px]">
         <h1 className="title text-2xl lg:text-3xl text-[#F04D79]">
-          {t("home.title")}
+          Discover the new Body Milk by Sampling Innovations
         </h1>
-        <p>{t("home.description")}</p>
+        <p>
+          The innovative body lotion that deeply penetrates the skin's surface:
+        </p>
         <ul className="list-inside pl-1 space-y-5">
-          <li>{t("home.benefits.hydration")}</li>
-          <li>{t("home.benefits.vitamin")}</li>
-          <li>{t("home.benefits.vegan")}</li>
+          <li>
+            💧 Provides up to 48h of hydration thanks to its fresh and light
+            texture, while nourishing and illuminating the skin.
+          </li>
+          <li>
+            🍊 Formulated with Vitamin C, the most powerful antioxidant
+            ingredient.
+          </li>
+          <li>🌱 Vegan product with 98% natural ingredients.</li>
         </ul>
-        <p>{t("home.perfume")}</p>
+        <p>
+          Enjoy the fruity embrace with this TOUS Electro Touch perfume with
+          100ml spray.
+        </p>
       </article>
 
       <article className="flex flex-col gap-5 mx-5 justify-center lg:items-center sm:mx-[100px] lg:mx-[150px]">
         <h2 className="title text-2xl lg:text-3xl text-[#F04D79]">
-          {t("home.win.title")}
+          Win a free product bundle!
         </h2>
-        <p>{t("home.win.description")}</p>
+        <p>
+          Answer a brief survey about your experience with the product to enter
+          the draw for a Sampling Innovations product bundle.
+        </p>
         <Link
           className="title-bold active-button w-full sm:w-auto bg-[#F04D79] rounded-md flex items-center shadow hover:shadow-md justify-center text-white font-semibold uppercase py-3 px-10 transition-shadow"
-          to={`/${currentLang}/form`}
+          to="/form"
         >
-          {t("home.win.cta")}
+          I want to participate
         </Link>
       </article>
     </section>
